eecde73bca70a127a31fed9a565b371a232ed7ca
4-alok <kumaralok344@gmail.com>
2021-07-16 13:43:48 +0530

    using lingpipe

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,46 +1,42 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
 import 'package:sentence_detection/sentence_detection.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
 
-class _MyAppState extends State<MyApp> {
-  final String s =
+  final String text =
       "Usually Sentence Detection is done before the text is tokenized and that's the way the pre-trained models on the web site are trained, but it is also possible to perform tokenization first and let the Sentence Detector process the already tokenized text. The OpenNLP Sentence Detector cannot identify sentence boundaries based on the contents of the sentence. A prominent example is the first sentence in an article where the title is mistakenly identified to be the first part of the first sentence. Most components in OpenNLP expect input which is segmented into sentences.";
-
-  Future<void> initPlatformState() async {
-    try {
-      List<String> k = await SentenceDetection.platformVersion(s);
-      k.forEach((element) {
-        print(element);
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Sentence Detection'),
         ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              initPlatformState();
-            },
-            child: Text("F1"),
-          ),
+        body: Container(
+            child: FutureBuilder<List<String>>(
+          future: SentenceDetection.getSentences(text),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return list(snapshot.data!);
+            } else
+              return Container();
+          },
+        )),
+      ),
+    );
+  }
+
+  ListView list(List<String> sentences) {
+    return ListView.builder(
+      itemCount: sentences.length,
+      itemBuilder: (context, index) => Card(
+        child: ListTile(
+          title: Text(sentences[index]),
         ),
       ),
     );
