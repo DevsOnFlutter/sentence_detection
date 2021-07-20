7016df56dd8f88188bc0306a4b0a022c46ab0387
4-alok <kumaralok344@gmail.com>
2021-07-20 15:06:39 +0530

    removed unused import, log

--- android/src/main/java/com/devsonflutter/sentence_detection/sentence_detection/SentenceDetectionPlugin.java
+++ android/src/main/java/com/devsonflutter/sentence_detection/sentence_detection/SentenceDetectionPlugin.java
@@ -1,47 +1,22 @@
 package com.devsonflutter.sentence_detection.sentence_detection;
-
-import android.content.Context;
-import android.content.res.AssetManager;
-
 import androidx.annotation.NonNull;
-
 import com.aliasi.sentences.MedlineSentenceModel;
 import com.aliasi.sentences.SentenceModel;
 import com.aliasi.tokenizer.IndoEuropeanTokenizerFactory;
 import com.aliasi.tokenizer.Tokenizer;
 import com.aliasi.tokenizer.TokenizerFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Properties;
-
-//import edu.stanford.nlp.pipeline.CoreDocument;
-//import edu.stanford.nlp.pipeline.CoreSentence;
-//import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import io.flutter.Log;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-//import opennlp.tools.sentdetect.SentenceDetectorME;
-//import opennlp.tools.sentdetect.SentenceModel;
-
-
 
 /** SentenceDetectionPlugin */
 public class SentenceDetectionPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
-  private Context context;
+//  private Context context;
 
   static final TokenizerFactory TOKENIZER_FACTORY = IndoEuropeanTokenizerFactory.INSTANCE;
   static final SentenceModel SENTENCE_MODEL  = new MedlineSentenceModel();
@@ -50,7 +25,7 @@ public class SentenceDetectionPlugin implements FlutterPlugin, MethodCallHandler
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "sentence_detection");
     channel.setMethodCallHandler(this);
-    context = flutterPluginBinding.getApplicationContext();
+//    context = flutterPluginBinding.getApplicationContext();
   }
 
   @Override
@@ -78,15 +53,15 @@ public class SentenceDetectionPlugin implements FlutterPlugin, MethodCallHandler
 
       List<String> sentences = new ArrayList<String>();
 
-      for (int i = 0; i < sentenceBoundaries.length; ++i) {
-        sentEndTok = sentenceBoundaries[i];
-        System.out.println("SENTENCE "+(i+1)+": ");
+      for (int sentenceBoundary : sentenceBoundaries) {
+        sentEndTok = sentenceBoundary;
+//        System.out.println("SENTENCE "+(i+1)+": ");
         StringBuilder sentOut = new StringBuilder();
-        for (int j=sentStartTok; j<=sentEndTok; j++) {
+        for (int j = sentStartTok; j <= sentEndTok; j++) {
           sentOut.append(tokens[j]).append(whites[j + 1]);
         }
         sentences.add(sentOut.toString());
-        sentStartTok = sentEndTok+1;
+        sentStartTok = sentEndTok + 1;
       }
         result.success(sentences);
     }
